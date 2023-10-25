d9fda61cfbc8f428a20262b45f336492b7e78c36
Ederson-RL <edersonramirezlopez@gmail.com>
2023-10-25 00:45:24 -0500

    Coneccion a base de datos

--- Trimestre 6/proyectohyg/proyectohyg/settings.py
+++ Trimestre 6/proyectohyg/proyectohyg/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import os
+from . import db as db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,7 @@ WSGI_APPLICATION = 'proyectohyg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db.MYSQL
 
 
 # Password validation
