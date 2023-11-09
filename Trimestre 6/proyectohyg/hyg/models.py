c74fc1b3e1dd81b898d6dee32a747ef14a8f1234
Ederson-RL <edersonramirezlopez@gmail.com>
2023-11-09 03:47:53 -0500

    Admin cruds

--- Trimestre 6/proyectohyg/hyg/models.py
+++ Trimestre 6/proyectohyg/hyg/models.py
@@ -13,24 +13,25 @@ class Category(models.Model):
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
         db_table = 'Categoria'
-        ordering = ['id'] 
+        ordering = ['id']
 
 
 class Products(models.Model):
     price = models.IntegerField()
     code = models.CharField(max_length=255, null=True)
     amount = models.FloatField(null=True)
+    # Vuelve a utilizar el nombre "name"
     name = models.CharField(max_length=255, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nombre
-    
+        return self.name  # Mantén el nombre del campo como "name"
+
     class Meta:
-            verbose_name = 'Producto'
-            verbose_name_plural = 'Productos'
-            db_table = 'Producto'
-            ordering = ['id'] 
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        db_table = 'Producto'
+        ordering = ['id']
 
 
 class Suppliers(models.Model):
@@ -38,31 +39,34 @@ class Suppliers(models.Model):
         max_length=255, null=True, blank=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     phone = models.CharField(max_length=255, null=True, blank=True)
-    
+
     def __str__(self):
         return self.nombre
-    
+
     class Meta:
         verbose_name = 'proveedor'
         verbose_name_plural = 'proveedores'
         db_table = 'proveedor'
-        ordering = ['id'] 
-    
+        ordering = ['id']
+
+
 class Compra(models.Model):
-#    amount = cantidad, suppliers = proveedores
+    #    amount = cantidad, suppliers = proveedores
     amountc = models.IntegerField()
-    datec= models.DateField(null=True, blank=True)
+    datec = models.DateField(null=True, blank=True)
     pricec = models.IntegerField()
     productc = models.CharField(max_length=255, null=True, blank=True)
     suppliersc = models.CharField(max_length=255, null=True, blank=True)
-    supplier = models.ForeignKey(Suppliers, on_delete=models.SET_NULL, null=True)
+    supplier = models.ForeignKey(
+        Suppliers, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
-    
+
     class Meta:
         verbose_name = 'compra'
         verbose_name_plural = 'compras'
         db_table = 'compras'
-        ordering = ['id'] 
+        ordering = ['id']
+
 
 class Gastos(models.Model):
     description = models.CharField(max_length=255, null=True)
@@ -73,12 +77,7 @@ class Gastos(models.Model):
         return self.descripcion
 
     class Meta:
-            verbose_name = 'gasto'
-            verbose_name_plural = 'gastos'
-            db_table = 'gastos'
-            ordering = ['id'] 
-
-
-
-
-
+        verbose_name = 'gasto'
+        verbose_name_plural = 'gastos'
+        db_table = 'gastos'
+        ordering = ['id']
